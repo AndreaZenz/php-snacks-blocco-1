53fae272976d4a3d537b60dbcb6c7957115f22db
AndreaZenz <zenzero.ere@gmail.com>
2021-06-05 11:15:27 +0200

    .

--- snack4.php
+++ snack4.php
@@ -11,4 +11,32 @@ function randomGen($min, $max, $quantity) {
 
 print_r(randomGen(0,15,16)); //generates 20 unique random numbers
 
+for($i = 0 ; $i < 15; $i++){
+
+    $valueBox = [];
+    $randomNumb = randomGen(0,15,16);
+
+    array_push($valueBox, $randomNumb);
+}
+
+var_dump($valueBox) ;
+
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <ul>
+    </ul>
+    </div>
+</body>
+</html>
